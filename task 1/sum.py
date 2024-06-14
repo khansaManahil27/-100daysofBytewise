8fa2dc9d5f7964a5a1959d5fb4cf2bf691faf305
khansaManahil27 <khansamanahil306@gmail.com>
2024-06-14 23:21:21 +0600

    Add files via upload

---
+++ task 1/sum.py
@@ -0,0 +1,11 @@
+# Get the first number from the user
+first_number = float(input("Enter first number: "))
+
+# Get the second number from the user
+second_number = float(input("Enter second number: "))
+
+# Calculate the sum
+sum_of_numbers = first_number + second_number
+
+# Print the sum
+print(f"The sum is: {sum_of_numbers}")
